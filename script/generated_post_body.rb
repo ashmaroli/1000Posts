87cbbdefff08621897fcd9265032d0d57fb3c517
Ashwin Maroli <ashmaroli@gmail.com>
2018-04-25 19:29:33 +0530

    use a minimal amount of regex in secondary script

--- script/generated_post_body.rb
+++ script/generated_post_body.rb
@@ -1,8 +1,8 @@
-# Get the generated markup from the path, extract just post-body without using
-# regex (y not? ;)) and re-align the markup.
+# Get the generated markup from the path, extract just post-body with minimal
+# regex and re-align the markup.
 # Finally print the markup!
 #
 puts File.read(File.expand_path(File.join(__dir__, "../_site/2018/04/24/hello-world.html")))
   .split('<div class="post-content e-content" itemprop="articleBody">')[1]
-  .split('</div><a class="u-url" href')[0]
+  .split(%r!\s*</div>\s*<a class="u-url" href!)[0]
   .strip.gsub("\n", "").gsub("</p><p>", "</p>\n\n<p>")
